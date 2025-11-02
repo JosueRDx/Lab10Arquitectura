f9ce5316231222c61fbbac9bfa81c3be37eb4d97
JosueRDx <168232189+JosueRDx@users.noreply.github.com>
2025-11-02 02:16:46 -0500

    fix: últimos ajustes antes de CQRS

--- Lab10_RodrigoApaza.Persistense/Program.cs
+++ Lab10_RodrigoApaza.Persistense/Program.cs
@@ -2,27 +2,20 @@ using Lab10_RodrigoApaza.Persistense.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticketera API Lab10 v1");
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication(); 
-app.UseAuthorization(); 
-
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
